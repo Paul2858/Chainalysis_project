62ad35ea5a9d265d4473a01f2081809513e7adc3
Paul2858 <pc2858@columbia.edu>
2021-11-07 11:39:34 -0500

    test

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import json
+from coinbase.wallet.client import Client
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def mainpage():
-	return 'TEST'
+    return render_template('main.html')
+
+
+def get_coinbase_prices(coin):
+	data = get_keys()
+	client = Client(data["coinbase_key"], ["coinbase_secret"])
+
+    return 0, 0
+
+
+def get_keys():
+    with open('secrets.json') as json_file:
+        data = json.load(json_file)
+    return data
